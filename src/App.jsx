566e1ee3163721427626c72dc717017a6db18d65
ImaFuentes10 <ima.fuentes.c@gmail.com>
2025-07-14 20:19:17 -0700

    Conecxión con API ollama

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
-import { BasicForm } from './components/BasicForm'
+import './index.css'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import axios from 'axios'
+import { useState } from 'react'
+
+const schema = yup.object({
+  userInput: yup
+    .string()
+    .min(3, 'El mensaje debe contener al menos 3 caracteres')
+    .required('El mensaje es obligatorio'),
+})
 
 export const App = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  })
+  // Guarda la respuesta de llama
+  const [response, setResponse] = useState('')
+  const [loading, setLoading] = useState(null)
+
+  const handlePrompt = async (data) => {
+    console.log(data.userInput)
+    setLoading(true)
+    try {
+      const res = await axios.post('http://localhost:11434/api/generate', {
+        model: 'llama2',
+        prompt: data.userInput,
+        stream: false
+      })
+      setResponse(res.data.response)
+    } catch (error) {
+      console.error('Error:', error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
-    <div>
-      <BasicForm />
-    </div>
+    <>
+      <form onSubmit={handleSubmit((handlePrompt))}>
+        <input
+          type='text'
+          {...register('userInput')}
+          className='w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400'
+        />
+        {errors.userInput && <p>{errors.userInput.message}</p>}
+        <button
+          className='w-full py-2 rounded transition cursor-pointer bg-blue-600 text-white hover:bg-blue-700'
+        >Preguntar
+        </button>
+      </form>
+      <div>
+        <p>{loading ? 'Generando respuesta' : response}</p>
+      </div>
+    </>
   )
 }
