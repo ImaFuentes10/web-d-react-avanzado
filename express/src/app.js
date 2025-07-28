10ff4752cf0d51d8ff4fe67b3e4167886aeba3cd
ImaFuentes10 <ima.fuentes.c@gmail.com>
2025-07-27 18:54:47 -0700

    API REST fianlizada

--- express/src/app.js
+++ express/src/app.js
@@ -1,60 +1,92 @@
-require('dotenv').config()
+// 1. Importar express y dotenv
 
-/* console.log(process.env.PORT)
-console.log(process.env.NOMBRE) */
+//  Common JS
+/* const express = require('express')
+require('dotenv').config() */
 
-// Importamos el módulo de Express
-const express = require('express')
+// ESModules
+import express from 'express'
+import dotenv from 'dotenv'
+import fs from 'fs'
 
-const { infoPeliculas } = require('./peliculas')
+dotenv.config()
 
-// Creamos una aplicación de Express
+// 2. Crear la aplicación de express
 const app = express()
-
-// Definimos el puerto que va a escuchar el servidor
 const PORT = process.env.PORT
+
+// Función que lee la info de db.json
+const readData = () => {
+  try {
+    const data = fs.readFileSync('./src/db.json')
+    return JSON.parse(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// Función que escribe dentro de db.json
+const writeData = (data) => {
+  try {
+    fs.writeFileSync('./src/db.json', JSON.stringify(data))
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('Hola mundo')
 })
 
-app.get('/api/peliculas', (req, res) => {
-  res.send(infoPeliculas)
+app.get('/peliculas', (req, res) => {
+  const data = readData()
+  res.json(data)
 })
 
-app.get('/api/peliculas/accion/:titulo/:year', (req, res) => {
-  const { titulo, year } = req.params
-  const results = infoPeliculas.accion.filter(pelicula => pelicula.titulo === titulo && pelicula.year === Number(year))
-
-  if (results.length === 0) {
-    return res.status(400).send(`No se encontraron resultados para ${titulo} en el año ${year}`)
-  }
-  res.send(results)
-})
-
-app.get('/api/peliculas/comedia/:country', (req, res) => {
-  const country = req.params.country
-  const results = infoPeliculas.comedia.filter(pelicula => pelicula.country === country)
-
-  if (req.query.ordenar === 'year') {
-    return res.send(results.sort((a, b) => b.year - a.year))
-  }
-
-  res.send(results)
+app.get('/peliculas/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const result = readData().action.find(pelicula => pelicula.id === id)
+  res.json(result)
 })
 
 app.use(express.json())
-app.post('/api/peliculas', (req, res) => {
-  const nuevaPelicula = req.body
 
-  console.log(nuevaPelicula)
+app.post('/peliculas', (req, res) => {
+  const data = readData()
+  const body = req.body
+  const newMovie = {
+    id: data.action.length + 1,
+    ...body
+  }
+  data.action.push(newMovie)
+  writeData(data)
+  res.json(newMovie)
+})
 
-  res.status(201).send({
-    message: 'Película recibida exitosamente',
-    data: nuevaPelicula
-  })
+app.put('/peliculas/:id', (req, res) => {
+  const data = readData()
+  const id = parseInt(req.params.id)
+  const body = req.body
+
+  const movieIndex = data.action.findIndex(movie => movie.id === id)
+  data.action[movieIndex] = {
+    ...data.action[movieIndex], // lo que ya existe
+    ...body // lo que quiero cambiar
+  }
+  writeData(data)
+  res.json({ message: 'Película actualizada correctamente' })
+})
+
+app.delete('/peliculas/:id', (req, res) => {
+  const data = readData()
+  const id = parseInt(req.params.id)
+
+  const movieIndex = data.action.findIndex(movie => movie.id === id)
+  data.action.splice(movieIndex, 1)
+  writeData(data)
+  res.json({ message: 'Película eliminada correctamente' })
 })
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+  console.log(`Servidor corriendo en puerto ${PORT}`)
 })
