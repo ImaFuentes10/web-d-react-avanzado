06f681d6bbf5cbfd704b405052cac763ab2bc7a6
ImaFuentes10 <ima.fuentes.c@gmail.com>
2025-07-29 19:02:37 -0700

    consumo Ollama desde backend

--- src/hooks/useOllama.js
+++ src/hooks/useOllama.js
@@ -1,19 +1,9 @@
 import axios from 'axios'
 
 export const useOllama = () => {
-  const sendMessage = async (userPrompt) => {
-    try {
-      const res = await axios.post('http://localhost:11434/api/generate', {
-        model: 'gemma3:4b',
-        prompt: userPrompt,
-        stream: false
-      })
-      return res
-    } catch (error) {
-      console.error('Error:', error)
-    } finally {
-      /* setLoading(false) */
-    }
+  const sendMessage = async (prompt) => {
+    const res = await axios.post('http://localhost:3001/api/chat', { prompt })
+    return res
   }
 
   return { sendMessage }
